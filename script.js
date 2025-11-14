0ca2a36edea39abe0ea1d57af03a29368adc750f
Deepanshu Aggarwal <deepanshu.aggarwal2005@gmail.com>
2025-11-14 20:36:50 +0530

    all change

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const container = document.querySelector(".container");
+// Smooth scrolling for navigation links
+document.querySelectorAll("nav ul li a").forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        const target = document.querySelector(e.target.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
 
-left.addEventListener("mouseenter", () =>
-  container.classList.add("hover-left")
-);
-left.addEventListener("mouseleave", () =>
-  container.classList.remove("hover-left")
-);
-right.addEventListener("mouseenter", () =>
-  container.classList.add("hover-right")
-);
-right.addEventListener("mouseleave", () =>
-  container.classList.remove("hover-right")
-);
+// Join Now button popup
+document.querySelector(".join-btn").addEventListener("click", () => {
+    alert("Thank you for your interest! Membership details coming soon.");
+});
